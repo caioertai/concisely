774291bd8bae4a92be5949e53e121f39860c101c
Caio Andrade <andrade@hey.com>
2021-03-08 16:36:56 -0600

    Add Lesson#available_hours_for

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -30,4 +30,33 @@ class Lesson < ApplicationRecord
       { from: start_time, to: end_time }
     end
   end
+
+  def available_hours_for(date)
+    hours_for_booking.reject do |time|
+      booked_times_for(date).include?(time)
+    end
+  end
+
+  private
+
+  def booked_times_for(date)
+    bookings
+      .for(date)
+      .pluck(:start_time)
+      .map(&:to_datetime)
+  end
+
+  def slots_count_possible_per_day
+    (12 * 60).minutes / duration.minutes
+  end
+
+  def hours_for_booking
+    slots_count_possible_per_day.floor.times.map do |n|
+      (beginning_of_work_day + (n * duration.minutes)).to_datetime
+    end
+  end
+
+  def beginning_of_work_day
+    Time.new(2021, 02, 03, 9, 0, 0, 0)
+  end
 end
